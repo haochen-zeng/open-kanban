1eb98496c6f680fc20d1811d7c9dee94797fae4d
EXSPIRAVIT_1104 <m18621006730@gmail.com>
2022-06-24 23:54:28 +0800

    improve
    added template file

--- addons/open_kanban/system/file.gd
+++ addons/open_kanban/system/file.gd
@@ -3,12 +3,50 @@ class_name OFS
 
 static func load_kanban() -> Dictionary:
 	var file = File.new()
+	if !Directory.new().file_exists("user://open-kanban/do not delete this 'data.json' file !!!.txt"):
+		var f = File.new()
+		file.open("user://open-kanban/do not delete this 'data.json' file !!!.txt", File.WRITE)
+		file.close()
+	
 	if file.open("user://open-kanban/data.json", File.READ) == OK:
 		var value = parse_json(file.get_as_text())
 		file.close()
 		return value
 	else:
-		return {}
+		return {
+			"version" : 0.1, 
+			"lists" : {
+				"0": {
+					"name": "Introduction",
+					"cards": {
+						"0": {"name": "Welcome to Open Kanban"},
+						"1": {"name": "Press the left mouse button to drag cards or lists,"},
+						"2": {"name": "Press the right mouse button to show the context menu"},
+						"3": {"name": "Press the setting button to change the language used in Open Kanban"},
+						"4": {"name": "づ￣ 3￣)づ"}
+					}
+				},
+				"1": {
+					"name": "ToDo",
+					"cards": {}
+				},
+				"2": {
+					"name": "Development",
+					"cards": {}
+				},
+				"3": {
+					"name": "Testing",
+					"cards": {}
+				},
+				"4": {
+					"name": "Done",
+					"cards": {}
+				}
+			}, 
+			"settings" : {
+				"lang" : "en"
+			}
+		}
 
 static func save_kanban(value : Dictionary) -> void:
 	var file = File.new()
